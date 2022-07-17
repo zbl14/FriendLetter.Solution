158cf55191d20ed56b608f3a780793b33a98e049
zbl14 <ifthereisoneday@gmail.com>
2022-07-16 23:12:19 -0700

    create FriendLetter by following the instructions

---
+++ FriendLetter/Program.cs
@@ -0,0 +1,22 @@
+// using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+// using FriendLetter.Models;
+
+namespace FriendLetter
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
+    }
+  }
+}
